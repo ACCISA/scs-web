2fea705b5054fc6405d77d65c552d01dc04b3b30
Romullus <39713625+ACCISA@users.noreply.github.com>
2022-11-02 01:55:29 -0400

    Add files via upload

--- scs_website-main/js/index.js
+++ scs_website-main/js/index.js
@@ -1,5 +1,5 @@
 // Select the contact link in the navbar, make it navigate to the contact section on the home page and apply smooth scrolling
 document.querySelector('#contact-link').addEventListener('click', e => {
     e.preventDefault();
-    document.querySelector('#contact').scrollIntoView({'behavior': 'smooth'});
+    document.querySelector('#contact-us-section').scrollIntoView({'behavior': 'smooth'});
 })
